c973c0596cf291e28ee755f0703760eaed8e100d
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2023-12-25 18:21:15 +0530

    dataTypes in js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+// Number
+let num = 42
+
+// String
+let name="Anurag"
+
+// Boolean
+let asian=false
+
+// undefined
+let age;
+
+// null (Object Type)
+let Degree=null
+
+// Symbols are unique and immutable values.
+let sym = Symbol('description');
+
+//Object:Represents a collection of key-value pairs
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+  };
