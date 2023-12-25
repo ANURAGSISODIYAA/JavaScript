ca60f8ab4ffd344e9ac92b31aa55d56d9335b35d
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2023-12-25 17:49:34 +0530

    variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+// Const variable can be change once defined
+const Accountname="Anurag";
+
+/* variables declared by let are only available inside the block
+ where they're defined. */
+ let AcoountEmail="anurag@gmail.com";
+
+ /* Variables declared by var are available 
+ throughout the function in which they're declared. */
+ var AccountType="Current";
+
+ //we can define a variable without a type 
+ AccountBranch="Indore"
+
+ //iF we dont assign a value it willgive as Undfined answer Althought Const must be initaliseed
+ let AccountPassword;
