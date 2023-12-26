2c6b9b6bd900223abc6bbbfc3a595c9f7ed9cbfe
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2023-12-26 08:57:52 +0530

    data conversion in js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log(typeof score)
+
+let valueToNumber = Number(score)
+console.log(typeof valueToNumber)
+
+// "33" --> 33
+// "33abc" --> NaN
+// true --> 1
+
+let isLogged = 1
+let booleanIsLogged = Boolean(isLogged)
+console.log(booleanIsLogged)
+
+// 1 ---> ture
+// "" ---> flase
+// "anu"---> true
+
+let num = 12
+let StringNum = String(num)
+console.log(typeof StringNum)
