2f9ea7f0dbfb4d777b0d0817efe78ff69911c4cd
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2023-12-26 09:20:37 +0530

    incrment

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -59,5 +59,11 @@ let gameCounter = 100
 ++gameCounter;
 console.log(gameCounter);
 
+/* If used postfix, with operator after operand (for example, x++), 
+the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++x), 
+the increment operator increments and returns the value after incrementing.*/
+
 // link to study
 // https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
